3e8d35ed103d408b34ddb5155efabc170d565efa
John Grover <d.rex@didymusrex.net>
2025-01-01 19:44:13 -0500

    added mqtt test programs

---
+++ mqtt_monitor.py
@@ -0,0 +1,39 @@
+import paho.mqtt.client as mqtt
+"""
+A simple, generic MQTT client to monitor MQTT commands and
+results
+"""
+
+"""
+MQTT values
+"""
+mqtt_broker = "mcc-lpl.local"
+mqtt_account = "mcc"
+mqtt_passwd = "mcc"
+mqtt_topic = "mcc/#"
+
+"""
+MQTT connect callback
+    Subscribing in on_connect() means that if we lose the connection and
+    reconnect then subscriptions will be renewed.
+"""
+def on_connect(client, userdata, flags, rc):
+    client.subscribe(mqtt_topic)
+
+"""
+MQTT receive message callback
+"""
+def on_message(client, userdata, msg):
+    print("message topic=",msg.topic)
+    print("message qos=",msg.qos)
+    print("message retain flag=",msg.retain)
+    print("-" * 40)
+
+mqc = mqtt.Client("monitor")
+mqc.on_connect = on_connect
+mqc.on_message = on_message
+
+mqc.username_pw_set(mqtt_account, mqtt_passwd)
+mqc.connect(mqtt_broker)
+
+mqc.loop_forever()
---
+++ mqtt_publisher.py
@@ -0,0 +1,37 @@
+import paho.mqtt.client as mqtt
+import sys
+from time import sleep
+"""
+A simple, generic MQTT client to send MQTT commands for testing
+"""
+
+"""
+MQTT values
+"""
+mqtt_broker = "mcc-lpl.local"
+mqtt_account = "mcc"
+mqtt_passwd = "mcc"
+
+topic = sys.argv[1]
+payload = sys.argv[2]
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code {}".format(rc))
+
+def on_publish(client, userdata, mid):
+    print("published message id {}".format(mid))
+ 
+mqc = mqtt.Client()
+mqc.on_connect = on_connect
+mqc.on_publish = on_publish
+
+mqc.username_pw_set(mqtt_account, mqtt_passwd)
+mqc.connect(mqtt_broker)
+
+mqc.loop_start()
+print("publishing topic {} payload {}".format(topic, payload))
+rc = mqc.publish(topic, payload, 0)
+
+sleep(5)
+print("rc = {} mid = {} is_published = {}".format(rc.rc, rc.mid, rc.is_published()))
+mqc.loop_stop()
