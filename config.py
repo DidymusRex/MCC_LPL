9d81586e754b8c0406557fa1cd33ad10c637aeec
John Grover <jwg@didymusrex.net>
2025-01-01 21:28:46 -0500

    updated config to ue mcc-lpl MQTT broker

--- config.py
+++ config.py
@@ -4,10 +4,10 @@ ssid_pw='open says me'
 
 mqtt_username='mcc'
 mqtt_password='mcc'
-mqtt_broker = 'jgrover-yoga.local'
+mqtt_broker = 'mcc-lpl.local'
 mqtt_port=1883
-mqtt_client_id = 'mcc-cthulu'
+mqtt_client_id = 'mcc-librarian'
 mqtt_subscribe_topic = b'mcc/#'
-mqtt_publish_topic = b'mcc/console/event/#'
+mqtt_publish_topic = b'mcc//#'
 
-sfx_location = '/home/jgrover/Projects/sfx/'
+sfx_location = '/home/jgrover/Projects/Miscatonic/MCC_LPL/sfx/'
