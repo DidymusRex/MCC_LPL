8cb817f3c21d384ef841243e0b663c96b97136fb
John Grover <d.rex@didymusrex.net>
2025-01-21 16:56:29 -0500

    updated mqtt server name

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ ssid_pw='open says me'
 
 mqtt_username='mcc'
 mqtt_password='mcc'
-mqtt_broker = 'mcc-lpl.local'
+mqtt_broker = 'mcc-league.local'
 mqtt_port=1883
 mqtt_client_id = 'mcc-librarian'
 mqtt_subscribe_topic = b'mcc/#'
