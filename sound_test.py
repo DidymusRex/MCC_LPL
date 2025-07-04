9863de29f6e5b49bb3b4ded78a312231a6ae1ea5
John Grover <d.rex@didymusrex.net>
2025-07-04 16:29:03 -0400

    added sound test

---
+++ sound_test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from config import *
+import os
+from pygame import mixer
+from subprocess import call
+import time
+
+# ------------------------------------------------------------------------------
+def play_sound(sound_file):
+        print(f'play_sound({sound_file})')
+
+        sfx = mixer.Sound(sfx_location + sound_file)
+        sfx.play()
+
+# ------------------------------------------------------------------------------
+def speak_text(text):
+        print('speaking text')
+        with open('temp.txt', 'w') as temp:
+                temp.write(text)
+
+        time.sleep(.5)
+        cmd='cat temp.txt|espeak -ven+f3 -g5 -s160'
+        call([cmd],shell=True)
+
+play_sound('334261__projectsu012__coin-chime.wav')
+speak_text('check check 1 2 3')
